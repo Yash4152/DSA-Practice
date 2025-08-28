da175174276b0179b99517370c802fef86cacf16
Yash4152 <yashmittal.cse25@jecrc.ac.in>
2025-08-28 22:49:47 +0530

    added first basic programs

---
+++ Basics/GradingSystem.java
@@ -0,0 +1,21 @@
+package Basics;
+
+import java.util.Scanner;
+
+public class GradingSystem {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int marks = sc.nextInt();
+        if(marks > 90){
+            System.out.println("Excellent");
+        }else if (marks > 80) {
+            System.out.println("Good");
+        }else if(marks>70){
+            System.out.println("fair");
+        }else if(marks>60){
+            System.out.println("Meets Expectations");
+        }else {
+            System.out.println("Fail");
+        }
+    }
+}
